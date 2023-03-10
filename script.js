cda777dd262ec6e418c2cae307523f99024b5aa9
NatalieLindholm <115688092+NatalieLindholm@users.noreply.github.com>
2023-03-10 16:31:40 +0200

    ?

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ function changeColor() {
     const blue = document.querySelector("#blue").value
 
     document.body.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')'
-}
 
+    // if (red > 255) {
+
+    // }
+
+    // if (green > 255) {
+
+    // }
+
+    // if (blue > 255) {
+
+    // }
+}
 
