2153be9fc17326f9d3614c2dbb55c220ff430006
NatalieLindholm <115688092+NatalieLindholm@users.noreply.github.com>
2023-03-10 09:17:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    const red = document.querySelector("#red").value
+    const green = document.querySelector("#green").value
+    const blue = document.querySelector("#blue").value
+
+    document.body.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')'
+}
+
+
