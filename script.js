310244da16be8943bd169b7bfc28fef4c86dbdf4
NatalieLindholm <115688092+NatalieLindholm@users.noreply.github.com>
2023-05-04 19:31:32 +0300

    done

--- script.js
+++ script.js
@@ -4,17 +4,5 @@ function changeColor() {
     const blue = document.querySelector("#blue").value
 
     document.body.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')'
-
-    // if (red > 255) {
-
-    // }
-
-    // if (green > 255) {
-
-    // }
-
-    // if (blue > 255) {
-
-    // }
 }
 
